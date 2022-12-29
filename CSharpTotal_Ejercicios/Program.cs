08a3243eb74a9fafde770b456f4579547021480b
Angel Javier Sanchez Tenjo <asanchez@xolit.com>
2022-12-29 03:03:02 -0500

    Agregar archivos de proyecto.

---
+++ CSharpTotal_Ejercicios/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpTotal_Ejercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
