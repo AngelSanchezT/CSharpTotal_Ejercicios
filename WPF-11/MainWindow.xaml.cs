0e0b087b744b254469748d86c438f0359e231d70
Angel Sanchez <ajsanchez22@gmail.com>
2023-01-28 17:02:01 -0500

    CheckBox

---
+++ WPF-11/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_11
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cbTodosHaCambiado (object sender, RoutedEventArgs e)
+        {
+            bool nuevoValor = (cbTodos.IsChecked == true);
+            cbChile.IsChecked = nuevoValor;
+            cbHongos.IsChecked = nuevoValor;
+            cbMuzzarella.IsChecked = nuevoValor;
+        }
+
+        private void cbSimpleHaCambiado (object sender, RoutedEventArgs e)
+        {
+            cbTodos.IsChecked = null;
+            if((cbChile.IsChecked == true) && (cbHongos.IsChecked == true) && (cbMuzzarella.IsChecked == true))
+            {
+                cbTodos.IsChecked = true;
+            }
+            if ((cbChile.IsChecked == false) && (cbHongos.IsChecked == false) && (cbMuzzarella.IsChecked == false))
+            {
+                cbTodos.IsChecked = false;
+            }
+        }
+    }
+}
